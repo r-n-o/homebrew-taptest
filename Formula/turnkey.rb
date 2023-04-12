ee4fd31af6acda1e6c65600d4eb72f5ba568eb9e
Arnaud Brousseau <rno@turnkey.io>
2023-04-11 20:41:32 -0700

    experiment with explicit rewrites

--- Formula/turnkey.rb
+++ Formula/turnkey.rb
@@ -9,7 +9,8 @@ class Turnkey < Formula
     sha256 "dab249ca4b1bd70ac92b1e403c68b44b4aafc96b76524be1a4484013566ac182"
 
     def install
-      bin.install "turnkey.darwin-aarch64" => "turnkey"
+        binfile_with_extension = File.basename(Dir.glob "#{prefix}/bin/turnkey*")
+        bin.install binfile_with_extension => "turnkey"
     end
   end
   if Hardware::CPU.intel?
@@ -17,7 +18,8 @@ class Turnkey < Formula
     sha256 "52ff1db2e8276f5a3adebf72b790dc3907490f317c1886e0b88af30fb958200b"
 
     def install
-      bin.install "turnkey.darwin-x86_64" => "turnkey"
+        binfile_with_extension = File.basename(Dir.glob "#{prefix}/bin/turnkey*")
+        bin.install binfile_with_extension => "turnkey"
     end
   end
 
